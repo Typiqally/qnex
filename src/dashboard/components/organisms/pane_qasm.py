14798494bcb440370859ec6e1edff6ff41e0422e
Jelle Maas <typiqally@gmail.com>
2025-01-14 21:11:35 +0100

    Add link to quantum circuit designers

--- src/dashboard/components/organisms/pane_qasm.py
+++ src/dashboard/components/organisms/pane_qasm.py
@@ -7,6 +7,23 @@ def create_pane_qasm(app):
     return dmc.Container(
         dmc.Stack([
             dmc.Title("OpenQASM", order=4),
+            dmc.Text(
+                [
+                    "For designing quantum circuits, you can use various quantum circuit designer tools such as ",
+                    dmc.Anchor(
+                        "IBM Composer",
+                        href="https://quantum.ibm.com/composer/",
+                        target="_blank",
+                    ),
+                    " and ",
+                    dmc.Anchor(
+                        "The Quantum Laend Quantum Circuit Simulator",
+                        href="https://thequantumlaend.de/quantum-circuit-designer/",
+                        target="_blank",
+                    ),
+                ],
+                size="sm"
+            ),
             create_qasm_input(app),
             dmc.Divider(variant="solid"),
         ]),
